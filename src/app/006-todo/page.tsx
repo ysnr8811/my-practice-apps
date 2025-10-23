9661514dbfe6a0c331b1dd1710b600fd80a5f5aa
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2025-10-23 20:31:47 +0900

    エラー修正

--- src/app/006-todo/page.tsx
+++ src/app/006-todo/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import CommonLayout from "../../components/CommonLayout";
-import { useSearchParams } from "next/navigation";
 import {Suspense} from "react";
 import ToDoUI from "@/app/006-todo/ToDoUI";
 
 export default function TodoPage() {
-  const searchParams = useSearchParams();
-  const label = searchParams.get("label") || "006-TODOリストアプリ";
 
   return (
       <CommonLayout>

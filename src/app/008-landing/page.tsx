405d584deea2cb749c2fb545f9ef5dd5d115f6bd
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2025-10-28 21:09:16 +0900

    エラー対応

--- src/app/008-landing/page.tsx
+++ src/app/008-landing/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import CommonLayout from "../../components/CommonLayout";
-import {useSearchParams} from "next/navigation";
 import {Suspense} from "react";
 import LandingUI from "@/app/008-landing/LandingUI";
 
 export default function LandingPage() {
-    const searchParams = useSearchParams();
 
     return (
         <CommonLayout>
